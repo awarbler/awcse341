9bea32e86b8353c1ae314c90cac5bc2a7f0323a6
awarbler <awoodfor@byui.edu>
2022-09-16 21:33:26 -0500

    edit console log

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = 3000
 app.use('/', require('./routes'))
 
 app.listen(port, () => {
-    console.log(`Example app listeningon port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
