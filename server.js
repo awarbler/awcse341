ce5cd2d271f0c45f4a0241b9dc3d92786a22281e
[awarbler] <[awoodfor@byui.edu]>
2022-09-22 17:18:43 -0500

    deleting db

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
+
 const express= require('express');
-
-const connectDB = require('./DB/connect');
-const app = express();
-
-
-connectDB();
-const port = process.env.Port || 3000
-
+const app = express()
+const port = 3000
 
 app.use('/', require('./routes'))
 
