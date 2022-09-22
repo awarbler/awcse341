2e93ace31f4a0b0b081faa40343a4d2c1fa7d5cf
[awarbler] <[awoodfor@byui.edu]>
2022-09-22 17:16:58 -0500

    fixing db errors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const routes = require('express').Router();
-const {showPersonName} = require("../controllers/contacts.controllers.js")
 
-routes.get ('/', showPersonName )
-//> {
-    //res.send("Hello, Anita! You are so beautiful!!")
-//});
+routes.get ('/', (req,res) => {
+    res.send("Hello, Anita! You are so beautiful!!")
+});
 
 module.exports = routes;
