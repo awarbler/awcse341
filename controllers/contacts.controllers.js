a45ade792c6719deb7ca60f6966d55c50f5857ea
[awarbler] <[awoodfor@byui.edu]>
2022-09-24 13:36:41 -0500

    added mongodb connected it and now it has crashed

--- controllers/contacts.controllers.js
+++
@@ -1,6 +0,0 @@
-const showPersonName = (req,res) => {
-    res.send("Hello, Anita! You are so beautiful!!")
-};
-
-module.exports = {showPersonName: showPersonName};
-// or modules.exports = showPersonName
--- server.js
+++ server.js
@@ -1,39 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
 
-const express= require('express');
-const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080;
+const app = express();
 
-app.use('/', require('./routes'))
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-// const express = require( 'express');
-// const bodyParser = require ('body-parser') ;
-// const mongodb = require('./db/connect') ;
-// E
-// const port = process. env. PORT | | 8080;
-// const app =
-// express();
-// app
-// •use (bodyParser.json())
-// .use((reg, res,
-// next)
-// {
-// res.setHeader( 'Access-Control-Allow-Origin', '*') ;
-// nextO);
-// 1)
-// .use('/', require('./routes')) ;
-// mongodb.initDb((err)
-// =
-// - {
-// if (err) {
-// console. log(err);
-// I else f
-// app. listen (port);
-// console. log("
-// "Connected to DB and listening on $(port}
-// -*);
-// }
-// });
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
