c1a61c4f8859344f4b93c4cfc345e190a0c1541c
[awarbler] <[awoodfor@byui.edu]>
2022-09-24 16:45:01 -0500

    editing routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+// const { index, show } = require("../controllers/contacts.controller");
 
 const contactsController = require('../controllers/contacts.controller');
 
 router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
+
+// List a single contact
+// router.use("/:id", show);
+
+// List all contacts
+// router.use("/", index);
 
 module.exports = router;
