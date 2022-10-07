bfc8cfa36e7ee32b86c431d20fad050fc89c2a0c
[awarbler] <[awoodfor@byui.edu]>
2022-10-07 11:36:17 -0500

    openCors

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const router = express.Router();
+//westons way
+const openCors = require("../middleware/openCors");
 
-router.use('/api-docs', require('./swagger'));
-router.use('/contacts', require('./contacts'));
+// const router = express.Router();
+
+// router.use('/api-docs', require('./swagger'));
+// router.use('/contacts', require('./contacts'));
+
+// weston
+const router = (router) => {
+  router.use([openCors, express.json()]);
+  router.use('/api-docs', require('./swagger'));
+  router.use('/contacts', require('./contacts'));
+  return router;
+};
 
 module.exports = router;
